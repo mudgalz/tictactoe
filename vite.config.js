19c8a3fc03c1ca07dc68ecc89ba81495a98aae09
mudgalz <akashmudgal66403@gmail.com>
2023-07-06 18:31:16 +0530

    vite.cjs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://mudgalz.github.io/tictactoe/",
+  base: "/tictactoe",
   plugins: [react()],
 });
